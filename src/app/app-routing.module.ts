57f28b7134c5302dfa40db7cf6c2e6079cf8e1bd
dewwwwlll <denukasachith@gmail.com>
2019-09-14 19:26:51 +0530

    Search + Search Results + SeatBooking (notfinal)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'search-results', pathMatch: 'full' },
+  { path: '', redirectTo: 'search-for-busses', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'search-results', loadChildren: './search-results/search-results.module#SearchResultsPageModule' },
+  { path: 'search-for-busses', loadChildren: './search-for-busses/search-for-busses.module#SearchForBussesPageModule' },
+
 
 ];
 
---
+++ src/app/search-for-busses/search-for-busses.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { SearchForBussesPage } from './search-for-busses.page';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+library.add(fas, far, fab);
+const routes: Routes = [
+  {
+    path: '',
+    component: SearchForBussesPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    FontAwesomeModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [SearchForBussesPage]
+})
+export class SearchForBussesPageModule {}
